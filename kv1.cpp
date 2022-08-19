5361c5a1ce88436b86fd83229bcaef5ffe9ba392
Waloid24 <111529670+Waloid24@users.noreply.github.com>
2022-08-19 20:31:20 +0300

    Update kv1.cpp

--- kv1.cpp
+++ kv1.cpp
@@ -2,53 +2,72 @@
 #include <ctype.h>
 #include <math.h>
 double vvod(void);
+int quad_eq (double a, double b, double c);
+double mass[2];
 int main (void)
-{
-	double a, b, c, d, x_1, x_2;
-	
-	printf("Kvadratnoe yrvnenie imeet vid:"
+{	
+	printf("The quadratic equation has the form:"
 		" a*(x^2) + b*x + c = 0\n");
 	printf("Enter a: ");
-	a = vvod();
+	double a = vvod();
 	printf("Enter b: ");
-	b = vvod();
+	double b = vvod();
 	printf("Enter c: ");
-	c = vvod();
+	double c = vvod();
 	printf("The equation is: "
 		"%.2lf*x^2 + %.2lf*x + %.2lf = 0\n", a,b,c);
 	
+	if (quad_eq(a,b,c))
+		if(mass[0] == mass[1])
+			printf("Solution: %lf.\n", mass[1]);
+		else
+			printf("Solutions: %lf, %lf.\n", mass[0], mass[1]);
+}
+int quad_eq (double a, double b, double c)
+{
+	extern double mass[2];
 	if (a == 0 && b == 0)
 	{
 		if (c == 0)
+		{
 			printf("x is any real number.\n");
+			return 0;
+		}
 		else
+		{
 			printf("The equation has no solutions.\n");
+			return 0;
+		}
 	}
 	
 	if (a == 0 && b != 0 && c != 0)
-		printf("The quadratic equation has 1 solution: x = %.2lf.\n", (-c)/b);
+	{
+		mass[0] = mass[1] = (-c)/b;	
+		return 1;	
+	}
+		
 	
 	if (a != 0)
 	{
-		d = b*b - 4*a*c;
+		double d = b*b - 4*a*c;
 		
 		if (d < 0)
 			printf("The equation has no solutions.\n");	
-		else if (d = 0)
-			printf("The quadratic equation has 1 solution: x = %.2lf.\n", (-b)/(2*a));	
+		else if (d>0)
+		{
+			mass[0] = (-b + sqrt(d)) / (2*a);
+			mass[1] = (-b - sqrt(d)) / (2*a);
+			return 1;
+		}
 		else
 		{
-			printf("Discriminant D = %.2lf.\n", d);
-			x_1 = (-b + sqrt(d)) / (2*a);
-			x_2 = (-b - sqrt(d)) / (2*a);
-			printf("The quadratic equation has 2 solutions: x_1 = %lf, "
-				"x_2 = %lf.\n", x_1, x_2);
+			mass[0] = mass[1] = (-b)/(2*a);
+			return 1;
 		}
 	}
 	
 	return 0;
 }
-
 double vvod (void)
 {
 	double a;
@@ -58,6 +77,6 @@ double vvod (void)
 		while (getchar() != '\n')
 			continue;
 	}
-	if (isdigit(a))
+	if (isdigit(a)) // если a - число, то
 		return a;
 }
